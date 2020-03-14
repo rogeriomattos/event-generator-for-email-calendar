49217e51ded1f82f9018f55f94b667cafdcb2e37
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-03-14 17:23:29 -0300

    google calendar

--- src/AddEventGoogleButton/index.jsx
+++ src/AddEventGoogleButton/index.jsx
@@ -1,26 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class AddEventGoogleButton extends React.Component {
-
-    /*
-    http://www.google.com/calendar/event?
-    action=TEMPLATE
-    &text=Summary+example
-    &dates=20191204T030000Z/20191206T120000Z
-    &details=Body+example
-    &location=S%C3%A3o+Paulo
+export class AddEventGoogleButton extends React.Component {    
     
-    "http://www.google.com/calendar/event?"+
-            "action=TEMPLATE"+
-            "&text="+event.summary+
-            "&dates="+event.startDateTime+"/"+event.endDateTime+
-            "&details="+event.body+
-            "&location="+event.location;
-    */
     render() {
+        const link = "http://www.google.com/calendar/event?action=TEMPLATE"
+                    +"&text="+this.formatText(this.props.title)
+                    +"&dates="+this.formatDate(this.props.isAllDay,this.props.dateStart,this.props.dateEnd)
+                    +"&details="+this.formatText(this.props.description)
+                    +"&location="+this.formatText(this.props.location);
+
         return (
-            <a href="http://www.google.com/calendar/event?action=TEMPLATE&text=Summary+example&dates=20191204T030000Z/20191206T120000Z&details=Body+example&location=S%C3%A3o+Paulo">Add event google</a>
+            <a href={link}>Add event google</a>
         );
     }
+
+    formatText = (text) => {
+        return text.replace(" ","+");
+    }  
+    
+    formatDate = (isAllDay,dateStart,dateEnd) =>{
+        var start ="", end="";
+        
+        if(dateStart)
+            start = this.toGoogleFormat(dateStart);
+        
+        if(!isAllDay  && dateEnd)
+            end = this.toGoogleFormat(dateEnd);
+        else{
+            
+            dateStart.setDate(dateStart.getDate()+1)
+            end = this.toGoogleFormat(dateStart);
+        }
+        return start + "/" + end;
+    }
+
+    toGoogleFormat = (dateProp) =>{
+
+        var iso = dateProp.toISOString();
+        iso = iso.replace(".000","");
+        iso = iso.replace(/[^a-zA-Z0-9]/g, "");
+        
+        return (iso).slice(0,15)+'Z';
+    }
 }
+
+
+AddEventGoogleButton.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    location: PropTypes.string,
+    dateStart: PropTypes.instanceOf(Date),
+    dateEnd: PropTypes.instanceOf(Date),
+    isAllDay:PropTypes.bool
+};
+
+AddEventGoogleButton.defaultProps = {
+    title: "",
+    description: "",
+    location: "",
+    dateStart:new Date(),
+    isAllDay:true
+};
+
 export default AddEventGoogleButton;
